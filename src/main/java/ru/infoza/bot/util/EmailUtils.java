574862fb48db973cfcef8e6a02b8f90c53d26935
berzerk <s.a.babrakov@gmail.com>
2024-01-16 20:32:28 +0100

    Fixed regexp for emails

--- src/main/java/ru/infoza/bot/util/EmailUtils.java
+++ src/main/java/ru/infoza/bot/util/EmailUtils.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 public class EmailUtils {
 
     private static final String EMAIL_PATTERN =
-            "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+            "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z-]{2,})$";
 
     private static final Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 
